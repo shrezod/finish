1abd0bbd8de0c03f1be5482b4043148bf366bf43
sherzod <serzodsiraliev@gmail.com>
2024-02-10 01:04:34 +0500

    Footer

---
+++ finish/src/components/Footer/index.jsx
@@ -0,0 +1,38 @@
+import "./style.scss"
+import { Link } from "react-router-dom"
+import FacebookColor from "../../assets/icons/facebookColor.png"
+import WKColor from "../../assets/icons/vkColor.png"
+import InstagramColor from "../../assets/icons/instColor.png"
+import Logo from "../../assets/icons/logoColor.png"
+import Mail from "../../assets/icons/mail.png"
+import Telegram from "../../assets/icons/tg.png"
+import Whatsapp from "../../assets/icons/wh.png"
+
+
+function Footer() {
+    return (
+        <div className="footer-wrapper">
+            <Link to={"/"}><img src={Logo} alt="Logo-color" /></Link>
+            <div className="links">
+                <Link to="/">Главная</Link>
+                <Link to="/excursions">Экскурсии</Link>
+                <Link to="/">Личный кабинет</Link>
+            </div>
+            <div className="contants">
+                <img src={Whatsapp} alt="whatsapp-icon" />+7 964 944 18 74 <br />
+                <img src={Telegram} alt="telegram-icon" />+7 918 919 98 28 <br />
+                <img src={Telegram} alt="telegram-icon" />Телеграм-бот PeroTravel <br />
+            </div>
+            <div className="icons">
+                <div className="mail">
+                    <img src={Mail} alt="mail-icon" /> <span>office@perotravel.ru</span>
+                </div>
+                <Link to={"https://vk.com/"}><img src={WKColor} alt="Wk" /></Link>
+                <Link to={"https://www.instagram.com/vdf_3.3"}><img src={InstagramColor} alt="Instagram" /></Link>
+                <Link to={"https://vk.com/"}><img src={FacebookColor} alt="Facebook" /></Link>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ finish/src/components/Reviews/index.jsx
@@ -0,0 +1,109 @@
+import "./style.scss"
+import ivan from "../../assets/images/ivan.png"
+import ivan2 from "../../assets/images/ivan2.png"
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+
+import 'swiper/scss';
+import 'swiper/scss/pagination';
+
+function Reviews() {
+    return (
+        <div className="reviews-wrapper">
+            <div className="top">
+                <span>Отзывы</span>
+                <span>Смотреть все</span>
+            </div>
+            <Swiper
+                slidesPerView={2}
+                spaceBetween={20}
+                pagination={{
+                    clickable: true,
+                }}
+                breakpoints={{
+                    230: {
+                        slidesPerView: 1.3,
+                        spaceBetween: 11,
+                    },
+                    430: {
+                        slidesPerView: 1.5,
+                        spaceBetween: 55,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                    }
+                }}
+                speed={300}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="container">
+                        <img src={ivan} alt="Reviews-images-1" />
+                        <div>
+                            <h3>Иван Иванов, 25 лет</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="container">
+                        <img src={ivan2} alt="Reviews-images-1" />
+                        <div>
+                            <h3>Иван Иванов, 25 лет</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="container">
+                        <img src={ivan} alt="Reviews-images-1" />
+                        <div>
+                            <h3>Иван Иванов, 25 лет</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="container">
+                        <img src={ivan2} alt="Reviews-images-1" />
+                        <div>
+                            <h3>Иван Иванов, 25 лет</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="container">
+                        <img src={ivan} alt="Reviews-images-1" />
+                        <div>
+                            <h3>Иван Иванов, 25 лет</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="container">
+                        <img src={ivan2} alt="Reviews-images-1" />
+                        <div>
+                            <h3>Иван Иванов, 25 лет</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                <div className="slider-controler">
+                    <div className="swiper-button-prev slider-arrow">
+                        <ion-icon name="arrow-back-outline"></ion-icon>
+                    </div>
+                    <div className="swiper-button-next slider-arrow">
+                        <ion-icon name="arrow-forward-outline"></ion-icon>
+                    </div>
+                </div>
+            </Swiper>
+        </div>
+    );
+}
+
+export default Reviews;
